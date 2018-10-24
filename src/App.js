adbc284f5060ff11e2a096d0c5654691d052ff1b
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-10-24 08:34:19 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+import "./styles/App.scss";
+
+export default class App extends Component {
+  render() {
+    return <div className="app">hiiiiiiii</div>;
+  }
+}
